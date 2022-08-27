4a30676b1422d7bba6791019ce101206aa50689a
Charlzk <104715127+Charlzk05@users.noreply.github.com>
2022-08-28 04:27:59 +0800

    Add files via upload

---
+++ Synapse X Remake/Worker/SelectedFolder.cs
@@ -0,0 +1,25 @@
+﻿namespace Synapse_X_Remake.Worker
+{
+    internal class SelectedFolder
+    {
+        public static string selectedPath = "./Scripts";
+
+        public SelectedFolder(string selectedPath)
+        {
+            SelectedPath = selectedPath;
+        }
+
+        private string SelectedPath
+        {
+            set
+            {
+                selectedPath = value;
+            }
+
+            get
+            {
+                return selectedPath;
+            }
+        }
+    }
+}
