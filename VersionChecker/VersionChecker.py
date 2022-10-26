0a8f8866ca5c9d5965445de7d8f5237d7a689b5c
Charlzk <104715127+Charlzk05@users.noreply.github.com>
2022-10-26 17:38:23 +0800

    Add files via upload

---
+++ VersionChecker/VersionChecker.py
@@ -0,0 +1,37 @@
+import requests, json
+from bs4 import BeautifulSoup
+
+def main():
+    response = requests.get("https://github.com/Charlzk05/Synapse-X-Remake-Synapse-X-Free-Version/tags")
+    soup = BeautifulSoup(response.text, "html.parser")
+    latest = soup.find_all("a", {"class":"Link--primary"})[0]
+    with open("./bin/RemakeVersion.txt", "r") as remakeVersion:
+        if (remakeVersion.read() == latest.text):
+            output = {
+                "Github": {
+                    "Latest Version": latest.text,
+                    "Release Link": f"https://github.com{latest['href']}"
+                },
+                "Message": "You're up to date",
+                "Status": "1"
+            }
+        else:
+            output = {
+                "Github": { 
+                    "Latest Version": latest.text,
+                    "Release Link": f"https://github.com{latest['href']}"
+                },
+                "Message": "You're not up to date",
+                "Status": "0"
+            }
+
+        json_object = json.dumps(output, indent=4)
+
+        with open("./bin/VersionCheck.json", "w") as output:
+            output.write(json_object)
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        print(str(e))
