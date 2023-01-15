d18c3c76e583c7d151020a04a63fc3c04521fae5
Jaemin Na <121419951+Jaeminah@users.noreply.github.com>
2023-01-15 10:51:29 +0700

    Update VersionChecker.py

--- VersionChecker/VersionChecker.py
+++ VersionChecker/VersionChecker.py
@@ -5,7 +5,7 @@ def main():
     response = requests.get("https://github.com/Jaeminah/repgbfgrtgerfdwffdcgefr/tags")
     soup = BeautifulSoup(response.text, "html.parser")
     latest = soup.find_all("a", {"class":"Link--primary"})[0]
-    with open("./bin/RemakeVersion.txt", "r") as remakeVersion:
+    with open("./bin/Version.txt", "r") as remakeVersion:
         if (remakeVersion.read() == latest.text):
             output = {
                 "Github": {
